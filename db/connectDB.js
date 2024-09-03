f59686cd2a092f092a4aca0ba888f21ece0cc0c5
tanyadubey9 <tanyadubey5005@gmail.com>
2024-09-03 12:01:10 +0530

    change

--- db/connectDB.js
+++ db/connectDB.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(`mongodb://localhost:27017/chai`, {
-      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
     return conn;
