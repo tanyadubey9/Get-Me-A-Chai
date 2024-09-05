9f5706585600f16b494796969412a4002b4fe6d5
tanyadubey9 <tanyadubey5005@gmail.com>
2024-09-05 19:36:20 +0530

    changes

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,14 +1,21 @@
 import mongoose from "mongoose";
 
+let isConnected = false; // Track the connection status
+
 const connectDB = async () => {
+  if (isConnected) {
+    console.log("MongoDB is already connected");
+    return;
+  }
+
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
+    isConnected = true;
     console.log(`MongoDB Connected: ${conn.connection.host}`);
-    return conn;
-  } catch (error) { 
-    console.error(error.message);
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
     process.exit(1);
   }
-}
+};
 
 export default connectDB;
